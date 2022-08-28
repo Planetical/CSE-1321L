9036e21fc7bf0101cc1b8ffc2517f87727ada430
Adam <53327121+Planetical@users.noreply.github.com>
2022-08-28 15:52:57 -0400

    Update Lab2B.java

--- Lab2B.java
+++ Lab2B.java
@@ -8,12 +8,12 @@ Lab#: 2B
  */
 public class Lab2B {
     public static void main(String[] args){
-        System.out.println("____*____");
-        System.out.println("___*_*___");
-        System.out.println("__*_*_*__");
-        System.out.println(" *_*_*_*");
-        System.out.println("__*_*_*__");
-        System.out.println("___*_*___");
-        System.out.println("____*____");
+        System.out.println("___*___");
+        System.out.println("__*_*__");
+        System.out.println("_*_*_*_");
+        System.out.println("*_*_*_*");
+        System.out.println("_*_*_*_");
+        System.out.println("__*_*__");
+        System.out.println("___*___");
     }
 }
