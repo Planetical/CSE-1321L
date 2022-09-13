b2a3722cfa7d0a7ef03195439ae061047071e15b
Adam <53327121+Planetical@users.noreply.github.com>
2022-09-13 17:18:02 -0400

    Update Lab5C.java

--- Lab5C.java
+++ Lab5C.java
@@ -1,3 +1,11 @@
+/*
+Class: 1321L
+Section: WJ1
+Term: Fall 2022
+Instructor: Nick Murphy
+Name: Adam Hutcheson
+Lab#: 5C
+ */
 import java.util.Scanner;
 public class Lab5C {
     public static void main(String[] args) {
