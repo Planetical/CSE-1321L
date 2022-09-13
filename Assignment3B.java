4aa3a2c94c62dd87fa9b058999c9cffa93392cf9
Adam <53327121+Planetical@users.noreply.github.com>
2022-09-13 14:41:58 -0400

    Update Assignment3B.java

--- Assignment3B.java
+++ Assignment3B.java
@@ -1,3 +1,11 @@
+/*
+Class: CSE 1321L
+Section: WJ1
+Term: Fall 2022
+Instructor: Nick Murphy
+Name: Adam Hutcheson
+Assignment#: 3B
+ */
 import java.util.Scanner;
 public class Assignment3B {
     public static void main(String[] args) {
