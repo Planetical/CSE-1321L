9a2ee2357e1c129c640cb68b89b04fa4f5c5ab89
Adam <53327121+Planetical@users.noreply.github.com>
2022-09-25 17:32:27 -0400

    Add files via upload

---
+++ Lab6A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lab6A {
+    public static void main (String [] args) {
+        System.out.println("Please enter 10 numbers and this program will display the largest.");
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i<=10; i++) {
+            System.out.println("Please enter number " + i + ":");
+            
+        }
+    }
+}
